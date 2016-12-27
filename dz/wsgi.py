8c6a81ab5c44a9a163fa3104f3e5b0479da04bcb
Alexander Ivannikov <a.v.ivannikov@ya.ru>
2016-12-27 11:42:27 +0300

    add gitignore

--- dz/wsgi.py
+++ dz/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DZ_WebBooking.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dz.settings")
 
 application = get_wsgi_application()
